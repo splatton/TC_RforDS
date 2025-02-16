56d310b876d4316e3c94304de172c75507795224
Tom Belanger <spltt.tlb@gmail.com>
2025-02-16 15:47:12 -0600

    adding lesson 15

--- LessonScripts/Lesson13.R
+++ LessonScripts/Lesson13.R
@@ -88,7 +88,7 @@ full_model <- lm(Volume ~ ., data = joined_data)
 
 #We can use step-wise feature selection to use the AIC to select our best features
 
-step(full_model, direction = "both")
+stats::step(full_model, direction = "both")
 
 #But weekday doesn't really make sense like this...nor does it make sense to include DOS
 
@@ -97,7 +97,7 @@ joined_data <- joined_data |>
 
 full_model <- lm(Volume ~ ., data = select(joined_data, -c(Campaign, DOS)))
 
-step(full_model)
+stats::step(full_model)
 
 #What does this tell us about our model?
 
@@ -138,3 +138,66 @@ for(i in 1:length(formula_list)) {
 weighted_model <- lm(Volume ~ Cost, data = joined_data, weights = DOS)
 
 #Could we make a better predictive model than this for guessing the volume at a facility?
+
+#What other variables might confound the relationship between Volume and Cost?
+
+joined_data <- joined_data |>
+  mutate(Month = factor(month(DOS)),
+         Season = case_when(Month %in% c(12,1,2) ~ "Winter",
+                            Month %in% c(3,4,5) ~ "Spring",
+                            Month %in% c(6,7,8) ~ "Summer",
+                            Month %in% c(9,10,11) ~ "Fall"))
+
+#Let's take a look at what we might expect to see by graphing our quantities
+
+joined_data |>
+  mutate(Season = fct(Season)) |>
+  group_by(Season) |>
+  summarize(Avg_Volume = mean(Volume, na.rm = TRUE)) |>
+  ggplot(aes(x = fct_reorder(Season, Avg_Volume), y = Avg_Volume, fill = Season)) +
+  geom_bar(stat = "identity")
+
+season_model <- lm(Volume ~ Cost + Season, data = joined_data)
+
+#What if we include the budget in the calculation instead of the cost?
+
+season_model <- lm(Volume ~ Budget + Season, data = joined_data)
+
+#What does this mean? Could we control for this seasonality in a different way?
+
+joined_data <- joined_data |>
+  group_by(DOS) |>
+  mutate(Total_Daily_Vol = sum(Volume)) |>
+  ungroup() |>
+  mutate(Other_Volume = Total_Daily_Vol - Volume) |>
+  na.omit()
+
+season_model <- lm(Volume ~ Budget + Other_Volume, data = joined_data)
+
+#How could we clarify this variable more?
+
+season_model <- lm(Volume ~ Budget + Other_Volume*Facility, data = joined_data)
+
+#Could we use seasons or months instead? Perhaps this would improve the predictive nature of our model?
+
+#Now let's explore possible nonlinear relationships in our data by looking at partial residuals
+
+terms <- predict(season_model, type = "terms")
+partial_resid <- residuals(season_model) + terms
+
+resid_tibble <- tibble(Budget = joined_data$Budget,
+                       Terms = terms[,'Budget'],
+                       PartialResid = partial_resid[,'Budget'])
+
+ggplot(resid_tibble, aes(x = Budget, y = PartialResid)) + geom_point(alpha = 0.2) + geom_smooth()
+
+#If we want to find points with high influence, we can use Cook's distance or hat values
+
+hatvalues(season_model)
+cooks.distance(season_model)
+
+#GAM usage
+
+library(mgcv)
+
+gam_model <- gam(Volume ~ s(Budget) + Other_Volume*Facility + s(Other_Volume), data = joined_data)
---
+++ LessonScripts/Lesson15.R
@@ -0,0 +1,121 @@
+#### Libraries -----------------------------------------
+
+library(tidyverse)
+library(tidymodels)
+
+#### Read the data ------------------------
+
+pt_volumes <- read_csv("pt_volumes.csv") |>
+  mutate(DOS = mdy(DOS)) |>
+  mutate(Monday = ifelse(wday(DOS) == 2, TRUE, FALSE),
+         Season = case_when(month(DOS) %in% c(12,1,2) ~ "Winter",
+                            month(DOS) %in% c(3,4,5) ~ "Spring",
+                            month(DOS) %in% c(6,7,8) ~ "Summer",
+                            .default = "Fall"))
+
+#We created some categorical variables to work with, here. Now, we'll try to see if a given day of the week is a Monday.
+
+#First, let's take a look at our data.
+
+pt_volumes |>
+  mutate(Weekday = as.factor(wday(DOS))) |>
+  ggplot(aes(x = Weekday, y = Volume, fill = Weekday)) +
+  geom_boxplot()
+
+monday_model <- glm(Monday ~ Volume + Season, pt_volumes, family = "binomial")
+
+summary(monday_model)
+
+#Now let's take a look at how to gather the coefficients
+
+exp(coef(monday_model))
+
+#What does this mean?
+
+#Can also use same techniques for logistic regression - splines, steps, interactions, etc
+
+monday_model <- glm(Monday ~ Volume*FacilityName, pt_volumes, family = "binomial")
+
+pt_volumes_pred <- pt_volumes |>
+  mutate(Probs = predict(monday_model, pt_volumes, type = "response"),
+         Log.Odds = predict(monday_model, pt_volumes)) 
+
+#Now let's calculate a lift curve for our predictions
+
+# Create deciles based on predicted probabilities
+pt_volumes_lift <- pt_volumes_pred %>%
+  mutate(decile = ntile(Probs, 10)) %>%  # Split into 10 groups
+  group_by(decile) %>%
+  summarise(
+    total = n(),                               # Total cases in each decile
+    actual_positives = sum(Monday),                 # Number of actual positives in each decile
+    avg_pred_prob = mean(Probs)       # Average predicted probability
+  )
+
+# Calculate overall proportion of positives in the dataset
+overall_positive_rate <- sum(pt_volumes_lift$actual_positives) / sum(pt_volumes_lift$total)
+
+# Calculate lift for each decile
+pt_volumes_lift <- pt_volumes_lift %>%
+  mutate(lift = (actual_positives / total) / overall_positive_rate)
+
+# Reverse order so highest probability decile is first
+pt_volumes_lift <- pt_volumes_lift %>% arrange(desc(decile))
+
+ggplot(pt_volumes_lift, aes(x = decile, y = lift)) +
+  geom_line(color = "blue", size = 1) +
+  geom_point(color = "red", size = 2) +
+  labs(title = "Lift Curve for Logistic Regression",
+       x = "Decile (1 = Highest Probability)",
+       y = "Lift (Proportion of True Positives vs. Baseline)") +
+  theme_minimal()
+
+#Can weight a model
+
+mean(pt_volumes_pred$Probs)
+
+wts <- ifelse(pt_volumes$Monday, round(1/(mean(pt_volumes$Monday))), 1)
+
+monday_model_wtd <- glm(Monday ~ Volume, pt_volumes, family = "binomial", weights = wts)
+
+mean(predict(monday_model, type = "response"))
+
+#Next we will use the yardstick package (part of tidymodels) to generate a confusion matrix
+
+pt_volumes_pred <- pt_volumes_pred |>
+  mutate(Wtd_Prob = predict(monday_model_wtd, type = "response")) |>
+  mutate(Predicted_Class = ifelse(Wtd_Prob >= 0.5, TRUE, FALSE)) |>
+  mutate(Predicted_Class = as.factor(Predicted_Class)) |>
+  mutate(Monday = as.factor(Monday)) |>
+  mutate(Weights = wts)
+
+pt_volumes_pred |>
+  conf_mat(truth = Monday, estimate = Predicted_Class) |>
+  print()
+
+#ROC curve
+
+# Compute ROC Curve data
+roc_curve_data <- pt_volumes_pred %>%
+  roc_curve(Wtd_Prob, truth = Monday)
+
+# Plot ROC Curve
+autoplot(roc_curve_data)
+
+pt_volumes_pred %>%
+  roc_auc(truth = Monday, Probs)
+
+#Let's use a slightly easier dataset to illustrate this
+
+mtcars$am <- factor(mtcars$am, labels = c("automatic", "manual"))
+
+mtcars_model <- glm(am ~ mpg + cyl, mtcars, family = "binomial")
+
+roc_curve_data <- mtcars %>%
+  mutate(Probs = predict(mtcars_model, type = "response")) |>
+  mutate(Probs = 1-Probs) |>
+  roc_curve(Probs, truth = am)
+
+# Plot ROC Curve
+autoplot(roc_curve_data)
+
